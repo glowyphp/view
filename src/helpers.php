44a3286b27001016fa9e444708645ec1965e068e
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-02-05 00:08:02 +0300

    refactor(view): improve codebase

--- src/helpers.php
+++ src/helpers.php
@@ -8,8 +8,10 @@ if (! function_exists('view')) {
     /**
      * Create a new view instance.
      *
-     * @param string $view Name of the view file
-     * @param array  $data Array of view variables
+     * @param string $view Name of the view file.
+     * @param array  $data Array of view variables.
+     * 
+     * @return View
      */
     function view(string $view, array $data = []): View
     {
@@ -22,8 +24,10 @@ if (! function_exists('e')) {
      * Escape string.
      *
      * @param string $string Name of the view file
-     * @param int    $int    A bitmask of one or more of the following flags, which specify how to handle quotes, invalid code unit sequences and the used document type.
+     * @param int    $flags  A bitmask of one or more of the following flags, which specify how to handle quotes, invalid code unit sequences and the used document type.
      *                       The default is ENT_COMPAT | ENT_HTML401.
+     * 
+     * @return string
      */
     function e(string $string, int $flags = ENT_COMPAT | ENT_HTML401): string
     {
